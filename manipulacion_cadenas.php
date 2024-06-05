d8483dbd02af830dfe8fe6b679df8ed7cacca3e7
Stephygrap09 <sstito03@tecnologicosudamericano.edu.ec>
2024-06-04 23:23:40 -0500

    Add files via upload
    Ejercicio 3: Uso de Cadenas
    Enunciado: Crea un archivo manipulacion_cadenas.php. Declara una variable con una
    cadena de texto. Utiliza funciones de PHP para cadenas como strlen, strtoupper,
    strtolower, strpos y substr para realizar diversas operaciones en la cadena. Muestra los
    resultados de cada operación en la página.

---
+++ manipulacion_cadenas.php
@@ -0,0 +1,11 @@
+<?php
+// Declara una variable con cadena de texto
+$cadena_de_ejemplo = "La vida es bella";
+echo "Cadena Original: <br>\n";
+echo $cadena_de_ejemplo;
+echo "<br>\nUsando strtolower: <br>\n";
+echo strtolower($cadena_de_ejemplo);
+echo "<br>\nUsando strtoupper: <br>\n";
+echo strtoupper($cadena_de_ejemplo);
+
+?>
